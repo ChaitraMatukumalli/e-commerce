829c924487601a1a7097e4fae733e121c89756d3
Nandini Atla <2300033835@kluniversity.in>
2024-06-30 21:23:48 +0530

    added changes in index.css , index.html and script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ searchform = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
    searchForm.classList.toggle('active');
 }
+let loginform = document.querySelector('.login-form-container')
+document.querySelector('#login-btn').onclick = () =>{
+    loginform.classList.toggle('active');
+}
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginform.classList.remove('active');
+}
  window.onscroll = () => {
     searchForm.classList.remove('active');
 
@@ -21,3 +28,25 @@ if(window.scrolly > 80){
             document.querySelector('.header.header-2').classList.remove('active');
             }
             } 
+            var swiper = new Swiper(".books-slider", {
+                loop:true,
+                centeredSlides: true,
+                autoplay:{
+                    delay:9500,
+                    disableOnInteraction : false,
+                },
+                breakpoints: {
+                  0: {
+                    slidesPerView: 1,
+                    s
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    
+                  },
+                  1024: {
+                    slidesPerView: 3,
+                    
+                  },
+                },
+              });
